741647a1bf91a7d957b15da8437a5a777efc1528
Kari O'Neal <triadfirewood@gmail.com>
2018-08-01 11:51:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
     sentence = sentence.split /\b/
-    new_sentence.each do |word| word.reverse
-    new_sentence.join 
+    sentence.each do |word| word.reverse
+    sentence.join 
 end
 end
 
