c29d4e6d5c973a61c91cf86b37dd8de049ad279f
Kari O'Neal <triadfirewood@gmail.com>
2018-08-01 11:18:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = []
   sentence.each_reverse! do |sentence|
-    new_sentence>>sentence.reverse!
+    new_sentence << sentence.reverse!
 end
 end
   
